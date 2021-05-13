9dfe0fccc9d3291d62b1c8349a0060d312c758f6
Zagham Muhmood <zaghampadda@gmail.com>
2021-05-14 02:25:13 +0400

    Added crud opreation for target

--- test/endpoints.js
+++ test/endpoints.js
@@ -1,17 +1,61 @@
 process.env.NODE_ENV = 'test'
 
-var test = require('ava')
-var servertest = require('servertest')
+const test = require('ava')
+const servertest = require('servertest')
 
-var server = require('../lib/server')
+const server = require('../lib/server')
 
 test.serial.cb('healthcheck', function (t) {
-  var url = '/health'
+  const url = '/health'
   servertest(server(), url, { encoding: 'json' }, function (err, res) {
     t.falsy(err, 'no error')
 
     t.is(res.statusCode, 200, 'correct statusCode')
     t.is(res.body.status, 'OK', 'status is ok')
+    t.end()
+  })
+})
+
+test.serial.cb('create target', function (t) {
+  const url = '/api/target'
+  servertest(server(), url, { method: 'POST', encoding: 'json' }, function (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    // t.is(res.body.status, 'OK', 'status is ok')
+    t.end()
+  })
+})
+
+test.serial.cb('get the target', function (t) {
+  const url = '/api/target/:id'
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    // t.is(res.body.status, 'OK', 'status is ok')
+    t.end()
+  })
+})
+
+test.serial.cb('get the target', function (t) {
+  const url = '/api/target/:id'
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    // t.is(res.body.status, 'OK', 'status is ok')
+    t.end()
+  })
+})
+
+test.serial.cb('get all targets', function (t) {
+  const url = '/api/targets'
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    // t.is(res.body.status, 'OK', 'status is ok')
     t.end()
   })
 })
